90c3835112bc2dc60893f335e0ea70bce5b43606
Amy Zhou <aymeighz@gmail.com>
2018-08-17 23:02:19 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,7 +7,6 @@ def reverse_each_word(sentence)
 end 
 
 def reverse_each_word(sentence)
-  sentence.split.collect {|word| word.reverse
-  end 
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end 
 
